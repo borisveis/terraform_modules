ecec6fa76c0e98098e94f369fecb69d1421cb29f
Boris Veis <boris.veis@gmail.com>
2025-03-30 14:56:33 -0700

    remote  codebuild_pipeline block

--- aws/codebuild/main.tf
+++ aws/codebuild/main.tf
@@ -102,47 +102,7 @@ resource "aws_codebuild_project" "codebuild_s3" {
 
 
 
-resource "aws_codebuild_project" "codebuild_pipeline" {
-  count = var.artifact_type == "CODEPIPELINE" ? 1 : 0
 
-  name           = var.name
-  description    = "CodeBuild project for ${var.name}"
-  build_timeout  = var.build_timeout
-  service_role   = var.service_role_arn
-  source_version = var.source_branch
-
-  dynamic "vpc_config" {
-    for_each = length(var.vpc_id) > 0 ? [ var.vpc_id ] : []
-
-    content {
-      vpc_id = var.vpc_id
-      subnets = var.vpc_subnets
-      security_group_ids = var.vpc_security_group_ids
-    }
-  }
-
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-
-  environment {
-    compute_type                = var.codebuild_computeType
-    image                       = var.codebuild_image
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
-    privileged_mode             = var.privileged_mode
-  }
-
-  source {
-    report_build_status  = false
-    insecure_ssl         = false
-    type                 = var.source_type
-    location             = var.source_location
-    buildspec            = var.buildspec
-    git_clone_depth      = var.source_type == "GITHUB" ? 1 : 0
-  }
-
-}
 resource "aws_codestarnotifications_notification_rule" "codebuild" {
   detail_type    = "BASIC"
   event_type_ids = [
