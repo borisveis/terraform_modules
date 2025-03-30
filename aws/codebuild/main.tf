593943f7afff1d8ff99be41410bba1a65b0d66c1
Boris Veis <boris.veis@gmail.com>
2025-03-30 15:20:33 -0700

    update codebuild

--- aws/codebuild/main.tf
+++ aws/codebuild/main.tf
@@ -9,6 +9,23 @@ resource "aws_codebuild_project" "codebuild" {
   service_role  = var.service_role_arn
   source_version = var.source_branch
 
+  # GitHub as the source
+  source {
+    type        = "GITHUB"               # GitHub as the source type
+    location    = var.source_location    # URL of the GitHub repo
+    buildspec   = var.buildspec          # Path to your buildspec file
+    git_clone_depth = 1                  # Limit git clone depth to the latest commit
+  }
+
+  # Environment block to define the build environment
+  environment {
+    compute_type                = var.codebuild_computeType  # Compute type (e.g., BUILD_GENERAL1_SMALL)
+    image                       = var.codebuild_image       # Docker image to use for the build
+    type                        = "LINUX_CONTAINER"         # Environment type
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = var.privileged_mode       # Docker-in-Docker for privileged builds
+  }
+
   dynamic "vpc_config" {
     for_each = length(var.vpc_id) > 0 ? [var.vpc_id] : []
 
@@ -19,11 +36,14 @@ resource "aws_codebuild_project" "codebuild" {
     }
   }
 
+  # Define artifacts (if any, based on the artifact_type variable)
   artifacts {
-    type = var.artifact_type == "NO_ARTIFACTS" ? "NO_ARTIFACTS" : var.artifact_type
-    location  = var.artifact_location
-    name      = var.artifact_name
-    path      = var.artifact_path
-    packaging = var.artifact_packaging
+    type                    = var.artifact_type == "NO_ARTIFACTS" ? "NO_ARTIFACTS" : var.artifact_type
+    location                = var.artifact_location
+    name                    = var.artifact_name
+    path                    = var.artifact_path
+    packaging               = var.artifact_packaging
     namespace_type         = var.artifact_namespace_type
-    override_artifact_name = var.art
+    override_artifact_name = var.artifact_override_name
+  }
+}
