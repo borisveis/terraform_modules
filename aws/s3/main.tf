dca103a1a4ba07df6a2cca8fec48275a3eda8ee9
Boris Veis <boris.veis@gmail.com>
2025-04-01 11:34:42 -0700

    remove acl

--- aws/s3/main.tf
+++ aws/s3/main.tf
@@ -1,15 +1,23 @@
-# Specify the AWS provider and region
 provider "aws" {
   region = var.region
 }
+
+# Create the S3 bucket
 resource "aws_s3_bucket" "test_s3_bucket" {
   bucket = var.bucket_name
-
-  tags = var.tags
+  tags   = var.tags
 }
 
-resource "aws_s3_bucket_acl" "test_s3_bucket_acl" {
+# Apply a bucket policy instead of an ACL
+resource "aws_s3_bucket_policy" "test_s3_bucket_policy" {
   bucket = aws_s3_bucket.test_s3_bucket.id
-  acl    = var.acl
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.test_s3_bucket.arn}/*"
+    }]
+  })
 }
-
