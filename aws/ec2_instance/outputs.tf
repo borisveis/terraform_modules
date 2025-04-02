9b0c2d2486a96754056dd12dffe5360fa625cb60
Boris Veis <boris.veis@gmail.com>
2025-04-01 17:15:35 -0700

    Update outputs.tf

--- aws/ec2_instance/outputs.tf
+++ aws/ec2_instance/outputs.tf
@@ -3,7 +3,7 @@ output "test_module_ip_public_ip" {
 }
 output "wev_ip_associate_public_ip_address" {
 
-  value = aws_instance.test_module.associate_public_ip_address
+  value = aws_instance.ec2_instance.associate_public_ip_address
 }
 output "default_subnet_id" {
   value = data.aws_vpc.aws_vpc.id
