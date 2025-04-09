f0c1049f88cd802c86231f2e07b8e7a96537887c
Boris Veis <boris.veis@gmail.com>
2025-04-09 12:06:56 -0700

    pipeline initial

--- aws/codepipeline/variables.tf
+++ aws/codepipeline/variables.tf
@@ -1,27 +1,14 @@
 variable "role_arn" {
-  description = "The ARN of the IAM role that CodePipeline assumes"
-  type        = string
+    type = string
 }
-
 variable "name" {
-  description = "The name of the CodePipeline"
-  type        = string
+    type = string
+    default = "default_name"
 }
-
 variable "artifact_store" {
   description = "Artifact store configuration for CodePipeline"
   type = object({
     location = string
     type     = string
   })
-}
-
-variable "codebuild_project_name" {
-  description = "The name of the CodeBuild project to use"
-  type        = string
-}
-
-variable "source_bucket" {
-  description = "The source S3 bucket for the pipeline"
-  type        = string
 }
